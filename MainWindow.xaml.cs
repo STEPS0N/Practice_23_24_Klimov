76557f728f9fe304a6ba8439b24e8a066c58442a
STEPS0N <stepan.klimoff@mail.ru>
2025-12-16 23:13:47 +0500

    Разработанное окно MainWindow. Климов

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,16 @@
-﻿using System.Text;
+﻿using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ClassConnection;
 
 namespace PhoneBook_Klimov
 {
@@ -16,9 +19,44 @@ namespace PhoneBook_Klimov
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static Connection connect;
+        
+        public static Pages.Main main;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            connect = new Connection();
+
+            connect.LoadData(Connection.tables.users);
+
+            connect.LoadData(Connection.tables.calls);
+
+            main = new Pages.Main();
+
+            OpenPageMain();
+        }
+        
+        public void OpenPageMain()
+        {
+            DoubleAnimation opgridAnimation = new DoubleAnimation();
+            opgridAnimation.From = 1;
+            opgridAnimation.To = 0;
+            opgridAnimation.Duration = TimeSpan.FromSeconds(0.6);
+            opgridAnimation.Completed += delegate
+            {
+                frame.Navigate(main);
+
+                DoubleAnimation doubleAnimation = new DoubleAnimation();
+                doubleAnimation.From = 0;
+                doubleAnimation.To = 1;
+                doubleAnimation.Duration = TimeSpan.FromSeconds(1.2);
+
+                frame.BeginAnimation(Frame.OpacityProperty, doubleAnimation);
+            };
+
+            frame.BeginAnimation(Frame.OpacityProperty, opgridAnimation);
         }
     }
 }
