923caa8ef77251f0b8e85572f7c7f29bf8377bf4
STEPS0N <stepan.klimoff@mail.ru>
2025-12-16 21:52:56 +0500

    Разработанная страница Main. Климов

---
+++ Pages/Main.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PhoneBook_Klimov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Main.xaml
+    /// </summary>
+    public partial class Main : Page
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Click_History(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Click_Phone(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+    }
+}
